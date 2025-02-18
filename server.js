749545bf8ad574978ec42050b87ce19cbfc67cf0
JacknS2520 <jackelly0221@gmail.com>
2025-02-18 07:31:46 -0600

    sign up and log-in added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3000;
@@ -23,14 +24,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
-  db.query(query, [username, password], (err, results) => {
+  const query = 'SELECT * FROM users WHERE username = ?';
+  db.query(query, [username], (err, results) => {
     if (err) throw err;
     if (results.length > 0) {
-      res.redirect('/dashboard.html');
+      const user = results[0];
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (result) {
+          res.redirect('/dashboard.html');
+        } else {
+          res.send('Incorrect username or password');
+        }
+      });
     } else {
       res.send('Incorrect username or password');
     }
+  });
+});
+
+app.post('/signup', (req, res) => {
+  const { username, password, email } = req.body;
+  const query = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) throw err;
+    db.query(query, [username, hash, email], (err, results) => {
+      if (err) throw err;
+      res.redirect('/');
+    });
   });
 });
 
